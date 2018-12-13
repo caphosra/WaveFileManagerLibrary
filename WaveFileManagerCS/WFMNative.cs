4bc30a0119d2325b6e2ee3eb9f84974e457d7deb
capra314cabra <capra314cabra@gmail.com>
2018-12-13 18:59:59 +0900

    Implemented 3 functions in CSharp

---
+++ WaveFileManagerCS/WFMNative.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace WFM
+{
+    namespace Native
+    {
+        public static class WaveFileManagerNative
+        {
+            [DllImport("wavefile_manager.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+            public static extern void createFile(string path, IntPtr prop);
+
+            [DllImport("wavefile_manager.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+            public static extern void generateWAVEFORMATEX(IntPtr format, EDataType type);
+
+            [DllImport("wavefile_manager.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+            public static extern void generateMusicProperty(IntPtr mpm, IntPtr waveFormat, EDataType type, IntPtr data, UInt32 size);
+        }
+    }
+}
---
+++ WaveFileManagerCS/WaveFileManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using WFM.Native;
+
+namespace WFM
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MusicProperty
+    {
+        public int m_FileSize;
+        public int m_PCMWAVEFORMAT_Size;
+        public WAVEFORMATEX m_WaveFormatEx;
+
+        public int m_DataSize;
+        // EDataType
+        public int m_DataType;
+        public IntPtr m_Data;
+    }
+
+    public enum EDataType
+    {
+        MONAURAL_8BITS = 1,
+        MONAURAL_16BITS = 2, 
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WAVEFORMATEX
+    {
+        public byte wFormatTag;
+        public ushort nChannels;
+        public uint nSamplesPerSec;
+        public uint nAvgBytePerSec;
+        public ushort nBlockAlign;
+        public ushort wBitsPerSample;
+    };
+
+    public static class WaveFileManager
+    {
+        public static void CreateFile(string path, MusicProperty prop)
+        {
+            IntPtr propPointer = Marshal.AllocHGlobal(Marshal.SizeOf(prop));
+            Marshal.StructureToPtr(prop, propPointer, false);
+            WaveFileManagerNative.createFile(path, propPointer);
+            Marshal.FreeHGlobal(propPointer);
+        }
+
+        public static void GenerateWAVEFORMATEX(ref WAVEFORMATEX format, EDataType dataType)
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(format));
+            Marshal.StructureToPtr(format, ptr, false);
+            WaveFileManagerNative.generateWAVEFORMATEX(ptr, dataType);
+            format = Marshal.PtrToStructure<WAVEFORMATEX>(ptr);
+            Marshal.FreeHGlobal(ptr);
+        }
+
+        public static void GenerateMusicProperty(ref MusicProperty prop, WAVEFORMATEX format, EDataType type, byte[] data)
+        {
+            IntPtr propPtr = Marshal.AllocHGlobal(Marshal.SizeOf(prop));
+            Marshal.StructureToPtr(prop, propPtr, false);
+
+            IntPtr formatPtr = Marshal.AllocHGlobal(Marshal.SizeOf(format));
+            Marshal.StructureToPtr(format, formatPtr, false);
+
+            IntPtr ptr = Marshal.AllocHGlobal(data.Length);
+            Marshal.Copy(data, 0, ptr, data.Length);
+
+            WaveFileManagerNative.generateMusicProperty(propPtr, formatPtr, type, ptr, (uint)data.Length);
+
+            prop = Marshal.PtrToStructure<MusicProperty>(propPtr);
+
+            Marshal.FreeHGlobal(propPtr);
+            Marshal.FreeHGlobal(formatPtr);
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+}
